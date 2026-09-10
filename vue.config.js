ee85f2d82a8d2010e15b754e7cc7a6525d5e2d78
tycon <Cubetan57@gmail.com>
2026-09-10 21:05:33 +0800

    fix: 渲染层无 require 时 HMR 报错，alias events 并关闭 hot

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
+const path = require('path')
+
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       preload: 'src/preload.js',
-      nodeIntegration: true
+      nodeIntegration: false
     }
+  },
+  chainWebpack: (config) => {
+    // nodeIntegration:false 时渲染层没有 require；webpack HMR 会 external "events" 调 require
+    // 强制打成浏览器版 events，避免白屏
+    config.resolve.alias.set('events', require.resolve('events/'))
+  },
+  devServer: {
+    // 关闭 HMR，进一步避开 electron-renderer 的 Node external 问题
+    hot: false,
+    liveReload: false
   }
 }
